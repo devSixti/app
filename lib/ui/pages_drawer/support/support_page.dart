423009d52d1254b63b01797f1b1b77775ca263d9
devlopera <devlopera@gmail.com>
2025-10-21 10:07:57 -0500

    Se actualizo la vista de soporte

--- lib/ui/pages_drawer/support/support_page.dart
+++ lib/ui/pages_drawer/support/support_page.dart
@@ -1,6 +1,6 @@
 import 'package:app/ui/pages_drawer/support/widget/contact_channels_widget.dart';
 import 'package:app/ui/pages_drawer/support/widget/faq_support_widget.dart';
-import 'package:app/ui/pages_drawer/support/widget/support_header_widget.dart';
+import 'package:app/ui/work_in_xisti/widgets/work_app_bar.dart';
 import 'package:app/ui/pages_drawer/support/widget/support_status_widget.dart';
 import 'package:flutter/material.dart';
 import '../../../core/theme/app_theme.dart';
@@ -16,15 +16,19 @@ class SupportPage extends StatelessWidget {
       body: CustomScrollView(
         physics: const BouncingScrollPhysics(),
         slivers: [
-          // Header
-          const SupportHeaderWidget(),
-          
+
+      
           // Contenido principal
           SliverToBoxAdapter(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: [
+                  const WorkAppBar(
+              title: 'Regresar',
+              showBack: true,
+            ),
+                  const SizedBox(height: 20), 
                   // Canales de contacto
                   const ContactChannelsWidget(),
                   
